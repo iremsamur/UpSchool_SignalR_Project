ee0fbd2e65ab537b76ab38507959f037afa77576
iremsmr <iremsamur129@gmail.com>
2023-01-06 19:01:51 +0300

    SignalR project is added

---
+++ UpSchool_SignalR_Api/Hubs/MyHub.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace UpSchool_SignalR_Api.Hubs
+{
+    public class MyHub : Hub
+    {
+        public static List<string> Names { get; set; } = new List<string>();
+
+        //isim gönderen bir metot yazalım.
+        public async Task SendName(string name)
+        {
+            Names.Add(name);
+            await Clients.All.SendAsync("ReceiveName",name);//gönderilen name değerini alır. ReceiveName olarak alıyor. parametreden gelen değeri alır.
+
+
+        }
+        public async Task GetNames()
+        {
+            //Buda isimleri getiren bir metot olacak
+            await Clients.All.SendAsync("ReceiveNames", Names);//listedeki tüm alanları getirir.
+        }
+    }
+}
---
+++ UpSchool_SignalR_Api/Startup.cs
@@ -0,0 +1,70 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UpSchool_SignalR_Api.Hubs;
+
+namespace UpSchool_SignalR_Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors();//cors için 
+            services.AddSignalR();//signal r için 
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            //cors izni için konfigürasyon
+            app.UseCors(x => x
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowCredentials()
+                .SetIsOriginAllowed(origin => true));
+
+            app.UseAuthorization();
+
+            //hub yönlendirmesi yazýlýr
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapHub<MyHub>("/MyHub");
+
+            });
+
+         
+            
+           
+        }
+    }
+}
